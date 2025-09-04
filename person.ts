9ab36fd4b81c9658fd24cf4bf1fbea393c738b92
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-04 14:17:11 +0200

    Refactor Person class to include first and last name, and enhance random person generation

--- person.ts
+++ person.ts
@@ -1,11 +1,37 @@
 import person from "./person.json" with { type: "json" };
+import { getRandomKey } from "./utils.ts";
 
 export class Person {
-  name: string;
+  firstName: string;
+  lastName: string;
   age: number;
 
-  constructor(name: string, age: number) {
-    this.name = name;
+  constructor(firstName: string, lastName: string, age: number) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.age = age;
+  }
+  
+  getName(): string {
+    const firstNameKey = getRandomKey(person.firstNames);
+    const firstName = person.firstNames[firstNameKey];
+
+    return this.firstName;
+  }
+  
+  getAge(): number {
+    return this.age;
+  }
+
+   static random(): Person {
+    
+    const firstNameKey = getRandomKey(person.firstNames);
+    const surNameKey = getRandomKey(person.surNames);
+    const ageKey = getRandomKey(person.ages);
+    const ageNumber = person.ages[ageKey];
+    const firstName = person.firstNames[firstNameKey];
+    const surName = person.surNames[surNameKey];
+
+    return new Person(firstName, surName, ageNumber);
   }
 }
