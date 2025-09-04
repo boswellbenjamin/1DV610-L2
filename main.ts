8376398d1e2cc7627d5a168d8a75ca84632f754f
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-04 21:23:38 +0200

    Log random person's name and age instead of the entire object

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ import { Person } from "./person.ts";
 
 const randomPerson = Person.random();
 
-console.log(randomPerson);
+console.log(randomPerson.getName());
+console.log(randomPerson.getAge());
