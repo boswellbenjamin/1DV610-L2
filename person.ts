c97ee5202ef6a8071ac7bafad143293b4da33149
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-04 13:10:55 +0200

    Start working on person class for mockl person data

---
+++ person.ts
@@ -0,0 +1,11 @@
+import person from "./person.json" with { type: "json" };
+
+export class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
