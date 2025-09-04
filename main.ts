dcf1efb5b9423a912c49a64d0943acf329636805
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-04 21:48:40 +0200

    add missing surname log for random person

--- main.ts
+++ main.ts
@@ -3,5 +3,6 @@ import { Person } from "./person.ts";
 const randomPerson = Person.random();
 
 console.log(randomPerson.getName());
+console.log(randomPerson.getSurname());
 console.log(randomPerson.getAge());
 console.log(randomPerson.getGender());
