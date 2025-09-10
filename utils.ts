18cede894b1989f0d6284ca0466f5a680d03b03d
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-10 10:32:21 +0200

    Add professionByAge function to determine profession based on age

--- utils.ts
+++ utils.ts
@@ -3,3 +3,15 @@ export function getRandomKey<T extends object>(obj: T): keyof T {
   const randomIndex = Math.floor(Math.random() * keys.length);
   return keys[randomIndex];
 }
+
+export function professionByAge(age: number): string {
+  if (age > 67) {
+    return "Retired";
+  } else if (age < 18 && age > 5) {
+    return "Student";
+  } else if (age <= 5) {
+    return "Baby";
+  } else {
+    return "";
+  }
+}
