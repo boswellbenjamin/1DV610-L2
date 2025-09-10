5bf9ee1e0f1dbe49098bc18cbccedfbf81308a4b
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-10 13:14:06 +0200

    Add build_npm.ts for packaging person data generator

---
+++ build_npm.ts
@@ -0,0 +1,31 @@
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./person.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: true,
+  },
+  test: false,
+  typeCheck: false,
+  package: {
+    name: "person-data-generator",
+    version: "0.1.0",
+    description: "Generate random person data for testing and development",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/boswellbenjamin/1DV610-L2",
+    },
+    bugs: {
+      url: "https://github.com/boswellbenjamin/1DV610-L2/issues",
+    },
+  },
+  postBuild() {
+    // Copy the JSON data file
+    Deno.copyFileSync("person.json", "npm/esm/person.json");
+    Deno.copyFileSync("person.json", "npm/script/person.json");
+  },
+});
